17b55a54fc1f3e34f52b6384743378e23a67cd59
sushil kumar <isushilchaudhary@gmail.com>
2023-07-16 01:11:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+import { fetchDataFromApi } from './utils/api'
+function App() {
+
+  useEffect(()=>{
+    apiTesting();
+  },[]);
+
+  const apiTesting = ()=>{
+    fetchDataFromApi('/movie/popular')
+    .then((res)=>{
+      console.log(res);
+    })
+  }
+  return (
+   
+
+      <div className='app'>App</div>
+  
+  )
+}
+
+export default App

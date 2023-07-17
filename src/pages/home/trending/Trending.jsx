c007e540689038d9b7eaa45c265bcb99125836a6
sushil kumar <isushilchaudhary@gmail.com>
2023-07-17 20:04:55 +0530

    fetch tredning data

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React,{useState} from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/UseFetch";
 
 const Trending = () => {
-    const onTabChange = ()=>{
 
+const [endpoint,setEndpoint]=useState("day");
+
+const {data,loading} = useFetch(`/trending/movie/${endpoint}`);
+
+    const onTabChange = (tab)=>{
+    setEndpoint(tab==="Day"?"day":"week");
     }
   return (
     <div className="carouselSction">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
-        <SwitchTabs data = {["Day","Week"]} onTabChange = {onTabChange}/>
+        <SwitchTabs data = {["Day","Week","Month"]} onTabChange = {onTabChange}/>
       </ContentWrapper>
     </div>
   );
